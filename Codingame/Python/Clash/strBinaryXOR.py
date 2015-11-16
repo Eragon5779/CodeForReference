0881a132bff37cabdbd0f4c44b8d19e5f13c05fc
NendoTaka <Klayton21@live.missouristate.edu>
2015-11-16 10:08:15 -0600

    Create strBinaryXOR.py

---
+++ Codingame/Python/Clash/strBinaryXOR.py
@@ -0,0 +1,16 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n_1, n_2 = input().split()
+for x in range(len(n_1)):
+    if (n_1[x] == '1' or n_2[x] == '1') and n_1[x] != n_2[x]:
+        print('1',end='')
+    else:
+        print('0',end='')
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr)
+
+
